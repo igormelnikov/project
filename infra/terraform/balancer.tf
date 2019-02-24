7d67aa20defb945215ab3df31a1c0904cb9e57f8
Igor Melnikov <prodwiththeprod@gmail.com>
2019-02-24 22:46:58 +0300

    HTTPS proxy for registry

--- infra/terraform/balancer.tf
+++ infra/terraform/balancer.tf
@@ -24,6 +24,21 @@ resource "google_compute_ssl_certificate" "default" {
 resource "google_compute_url_map" "default" {
   name            = "default"
   default_service = "${google_compute_backend_service.gitlab.self_link}"
+
+  host_rule {
+    hosts = ["imel-project.ml"]
+    path_matcher = "allpaths"
+  }
+
+  path_matcher {
+    name = "allpaths"
+    default_service = "${google_compute_backend_service.gitlab.self_link}"
+
+    path_rule {
+      paths = ["/registry"]
+      service = "${google_compute_backend_service.gitlab-registry.self_link}"
+    }
+  }
 }
 
 resource "google_compute_backend_service" "gitlab" {
@@ -47,6 +62,11 @@ resource "google_compute_instance_group" "gitlab" {
     name = "https"
     port = 443
   }
+
+  named_port {
+    name = "registry"
+    port = 4567
+  }
 }
 
 resource "google_compute_https_health_check" "gitlab" {
@@ -54,3 +74,14 @@ resource "google_compute_https_health_check" "gitlab" {
   request_path = "/-/health"
 }
 
+resource "google_compute_backend_service" "gitlab-registry" {
+  name      = "gitlab-registry"
+  port_name = "registry"
+  protocol  = "HTTPS"
+
+  backend {
+    group = "${google_compute_instance_group.gitlab.self_link}"
+  }
+
+  health_checks = ["${google_compute_https_health_check.gitlab.self_link}"]
+}
